0872413aa8ddac53c50eda72d51774cc89dbdf28
Vvk77 <vvk.yadav12@gmail.com>
2022-11-23 13:53:10 +0530

    updated: details

---
+++ index.js
@@ -0,0 +1,23 @@
+var btnTranslate = document.querySelector("#btn");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+
+
+
+
+function clickHandler () {
+outputDiv.innerText = "flg;gmgl" + txtInput.value;
+
+
+
+
+};
+
+
+
+
+
+
+
+btnTranslate.addEventListener("click", clickHandler)
