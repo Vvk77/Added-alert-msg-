b767b1ae7a376e5f743c6e94ebca5235f01cc0d5
Vvk77 <vvk.yadav12@gmail.com>
2022-11-23 18:16:30 +0530

    added:alert msg

--- index.js
+++ index.js
@@ -1,60 +1,32 @@
-var btnTranslate = document.querySelector("#btn");
+var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
 
-var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function getTranslationURL (text) {
- return serverURL + "?" + "text=" + text
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
 
 }
 
+function clickHandler() {
+    var inputText = txtInput.value;
+
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+           })
+
+
+           alert("something wrong with server! try again after some time")
 
 
 
 
+        };
 
-
-
-function clickHandler () {
-var inputText = txtInput.value;
-
-
-fetch(getTranslationURL(inputText))
-.then(response => response.json())
-.then (json => {
-    var translatedText = json.contents.traslated;
-    outputDiv.innerText = translatedText;
-
-
-
-
-
-
-})
-
-
-
-
-
-
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-btnTranslate.addEventListener("click", clickHandler)
+        btnTranslate.addEventListener("click", clickHandler)
