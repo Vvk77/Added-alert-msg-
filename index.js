5c081b1a1b21b9db677b26baacbab236cb2f3ba3
Vvk77 <vvk.yadav12@gmail.com>
2022-11-23 15:28:33 +0530

    update index.js

--- index.js
+++ index.js
@@ -3,16 +3,53 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
 
+var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+
+function getTranslationURL (text) {
+ return serverURL + "?" + "text=" + text
+
+}
+
+
+
+
+
 
 
 
 function clickHandler () {
-outputDiv.innerText = "flg;gmgl" + txtInput.value;
+var inputText = txtInput.value;
+
+
+fetch(getTranslationURL(inputText))
+.then(response => response.json())
+.then (json => {
+    var translatedText = json.contents.traslated;
+    outputDiv.innerText = translatedText;
+
+
+
+
+
+
+})
+
+
+
 
 
 
 
 };
+
+
+
+
+
+
+
+
+
 
 
 
